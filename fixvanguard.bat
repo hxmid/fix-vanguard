ced15650b68cba7b4876699ecc005926edf70d93
hamid <65052613+hxmid@users.noreply.github.com>
2024-10-05 03:32:20 +1000

    Add files via upload

---
+++ fixvanguard.bat
@@ -0,0 +1,37 @@
+:: https://github.com/techno-world/Command-Prompt/blob/master/Request%20Admin%20Privileges.bat
+@echo off
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if "%errorlevel%" NEQ "0" (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::--------------------------------------
+
+::ENTER YOUR CODE BELOW:
+
+taskkill /im vgtray.exe
+sc stop vgc
+sc delete vgc
+sc stop vgk
+sc delete vgk
+rmdir /s /q "C:\Program Files\Riot Vanguard"
+pause
+shutdown /r
